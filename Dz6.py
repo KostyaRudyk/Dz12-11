b747a19f563029eb5ae38880f15497b80fcfe163
KostyaRudyk <you@example.com>
2023-11-11 12:40:41 +0200

    first commit

--- Dz6.py
+++ Dz6.py
@@ -1,22 +1,19 @@
 result = []
-
 def divider(a, b):
     try:
         if a < b:
             raise ValueError("a should be greater than or equal to b")
         if b > 100:
-            raise IndexError("b should be less than or equal to 100")
+            raise IndexError("b should not be greater than 100")
         return a / b
     except ValueError as ve:
         print(f"ValueError: {ve}")
+        return None
     except IndexError as ie:
         print(f"IndexError: {ie}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
         return None
 
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
-
 for key in data:
     res = divider(key, data[key])
     if res is not None:
